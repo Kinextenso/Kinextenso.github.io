25669035f5da7b93a9690075391c24ac34adb644
sagaua17 <samuel.gaudreau17@gmail.com>
2017-11-13 23:14:33 -0500

    fixed

--- assets/js/main.js
+++ assets/js/main.js
@@ -266,16 +266,3 @@
 	});
 
 })(jQuery);
-
-
-function stickyNav() {
-	
-    var nav = document.getElementById("nav");
-    var sticky = nav.offsetTop;
-    
-    if (window.pageYOffset >= sticky) {
-        nav.classList.add("sticky")
-    } else {
-        nav.classList.remove("sticky");
-    }
-}
